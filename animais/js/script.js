bad7dc015b25368cac1f0968c1d71443804b2588
Fagner Tranquilino <fagnertds@gmail.com>
2024-08-13 16:22:13 -0300

    tooltip e modal

--- animais/js/script.js
+++ animais/js/script.js
@@ -2,8 +2,14 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimaçãoScroll from "./modules/scroll-animacao.js";
 import activeAcordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initScrollSuave();
 initAnimaçãoScroll();
 activeAcordion();
 initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
